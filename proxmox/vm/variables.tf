fd41c985ffdf266a4d40a85878dfeb0cf874f3b5
Adam Atyi <atyiadam@gmail.com>
2024-10-09 22:19:17 +0200

    Remove variables

--- proxmox/vm/variables.tf
+++ proxmox/vm/variables.tf
@@ -3,16 +3,16 @@ variable "pm_api_url" {
   type        = string
 }
 
-variable "pm_api_token_id" {
-  description = "Proxmox API Token ID"
-  type        = string
-}
-
-variable "pm_api_token_secret" {
-  description = "Proxmox API Token Secret"
-  type        = string
-  sensitive   = true
-}
+#variable "pm_api_token_id" {
+#  description = "Proxmox API Token ID"
+#  type        = string
+#}
+#
+#variable "pm_api_token_secret" {
+#  description = "Proxmox API Token Secret"
+#  type        = string
+#  sensitive   = true
+#}
 
 variable "api_token" {
   description = "Concatenated API token"
