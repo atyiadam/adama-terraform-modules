447b8e6a7680094e805c970ccf1b86feed478e76
Adam Atyi <atyiadam@gmail.com>
2024-10-09 23:00:56 +0200

    Remove provider

--- proxmox/vm/main.tf
+++ proxmox/vm/main.tf
@@ -1,24 +1,3 @@
-terraform {
-
-  required_version = "1.9.6"
-
-  required_providers {
-    proxmox = {
-      source  = "bpg/proxmox"
-      version = "0.66.1"
-    }
-  }
-
-}
-
-provider "proxmox" {
-
-  endpoint  = var.pm_api_url
-  api_token = var.api_token
-  insecure  = true
-
-}
-
 resource "proxmox_virtual_environment_vm" "test_vm" {
 
   name      = var.vm_name 
