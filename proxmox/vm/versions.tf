7af2d34bed1d14ea1c567e640f896323b3e353c1
Adam Atyi <atyiadam@gmail.com>
2024-10-28 20:55:59 +0100

    Pin version

--- proxmox/vm/versions.tf
+++ proxmox/vm/versions.tf
@@ -1,15 +1,15 @@
 terraform {
-  required_version = ">= 1.9.6"
+  required_version = "~> 1.9.6"
 
   required_providers {
     proxmox = {
       source  = "bpg/proxmox"
-      version = ">= 0.66.1"
+      version = "~> 0.66.1"
     }
 
     powerdns = {
       source  = "pan-net/powerdns"
-      version = ">= 1.5.0"
+      version = "~> 1.5.0"
     }
   }
 }
